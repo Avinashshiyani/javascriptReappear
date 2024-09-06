08c01699234251fa00e410089ff10e94b2e3974c
Avinash <avinashshiyani760@gmail.com>
2024-09-06 18:31:28 +0530

    Switch in js

---
+++ switch.js
@@ -0,0 +1,10 @@
+let a = "Helldsdsdo";
+
+switch (a) {
+  case "sads":
+    return console.log("not good brother");
+  case "Hello":
+    return console.log("Good brother You know Html and Js");
+  default:
+    return console.log("chalo niklooo please");
+}
