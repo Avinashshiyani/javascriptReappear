e59b125f8240c7721aeead0b2a88e983fca2b006
Avinash <avinashshiyani760@gmail.com>
2024-09-08 21:18:12 +0530

    Loops in js

---
+++ loops.js
@@ -0,0 +1,19 @@
+let a;
+
+// for (a = 0; a < 5; a++) {
+//   console.log(a/2);
+// }
+
+// for in loop
+
+// let object = {
+//   avinash: "hello my name is avinash",
+//   jay: "hello my name is jay",
+//   loop: "My name is Loop",
+// };
+
+// for (a in object) {
+//   console.log(" Names are " + a + " and title is " + object[a]);
+// }
+
+
