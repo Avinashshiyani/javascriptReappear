f799eaa9cd2809e56c455af536030486c0c54325
Avinash <avinashshiyani760@gmail.com>
2024-09-11 19:27:01 +0530

    Practice Test String

---
+++ practiceString.js
@@ -0,0 +1,15 @@
+let word = 'Hello"';
+
+// console.log(word.length);
+
+// console.log(word.startsWith("H"));
+// console.log(word.toLowerCase(word));
+
+// console.log(word.includes("a"));
+
+let sentence = "Please";
+
+// let extract = sentence.toLowerCase();
+
+// let th = sentence.slice(14);
+let change = sentence.slice
