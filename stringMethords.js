01563748be0211b52cba97d971c6e6fb05d56b45
Avinash <avinashshiyani760@gmail.com>
2024-09-11 15:44:18 +0530

    String Other Methords in Js

---
+++ stringMethords.js
@@ -0,0 +1,14 @@
+let name = "Avinash";
+
+let lenghtOfName = name.length;
+let upperCase = name.toUpperCase();
+let lowerCase = name.toLowerCase();
+let slice = name.slice(0, 4);
+let replace = name.replace("a", "hello");
+let concate = name.concat("hello");
+// console.log(concate);
+
+
+for(let i =0 ; i < lenghtOfName ; i++){
+    console.log(name[i])
+}
