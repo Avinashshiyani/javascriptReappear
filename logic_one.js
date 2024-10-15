cdb315543cc28a3f952b2dcbb881b1627d452963
Avinash <avinashshiyani760@gmail.com>
2024-10-15 17:51:16 +0530

    Logic in js

---
+++ logic_one.js
@@ -0,0 +1,11 @@
+function squareNumbers(arr) {
+    for(let i = 0 ; i < arr.length ; i++){
+        console.log(Math.sqrt(arr[i]))
+    }
+}
+
+squareNumbers([1 , 2 , 3])
+
+// Test your function
+// console.log(squareNumbers([1, 2, 3])); // Output: ?
+// console.log(squareNumbers([4, 5, 6])); // Output: ?
