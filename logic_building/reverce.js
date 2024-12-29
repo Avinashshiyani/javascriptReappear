7c1c720811b39d4ef0f4468008e50e9bf769c846
Avinash <avinashshiyani760@gmail.com>
2024-12-29 23:05:50 +0530

    Reverce Number in js

--- logic_building/reverce.js
+++ logic_building/reverce.js
@@ -2,7 +2,7 @@ let reverceNumber = (number = 123456) => {
   let initialNumber = String(number);
   let reverceNumber = initialNumber.split("").reverse().join("");
   let convertTonumber = Number(reverceNumber);
-  console.log(typeof convertTonumber)
+  console.log(convertTonumber)
 };
 
 reverceNumber();
---
+++ logic_building/reverce_number.js
@@ -0,0 +1,7 @@
+let reverceNumber = (number = 123456)=>{
+    let initialNumber = String(number).split("").reverse()
+    let answer = initialNumber
+    return console.log(answer)
+}
+
+reverceNumber(321568)
